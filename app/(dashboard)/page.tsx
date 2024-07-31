51a3323317a69b774e32ca9a08fc6efb62106bf4
Pedro Abreu de Azevedo <azevedodev@outlook.com>
2024-07-31 19:02:01 -0300

    feat: Create Account POST route created

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,18 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function Home() {
   const { data: accounts, isLoading } = useGetAccounts();
+  const { onOpen } = useNewAccount();
   if (isLoading) {
     return <div>Loading...</div>;
   }
   return (
     <div>
+      <Button onClick={onOpen}>New Account</Button>
       {accounts?.map((account) => <div key={account.id}>{account.name}</div>)}
     </div>
   );
