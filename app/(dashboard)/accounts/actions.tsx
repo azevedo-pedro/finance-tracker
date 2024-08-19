864757d05cf6907d3e6218fb996b2f5ebeee3b67
Pedro Abreu de Azevedo <azevedodev@outlook.com>
2024-08-19 19:29:21 -0300

    feat: add delete and edit account

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -8,16 +8,31 @@ import {
   DropdownMenuTrigger,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
-import { Edit, MoreHorizontal } from "lucide-react";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
+import { useConfirm } from "@/hooks/use-confirm";
 
 type Props = {
   id: string;
 };
 
 export function Actions({ id }: Props) {
+  const deleteMutation = useDeleteAccount(id);
   const { onOpen } = useOpenAccount();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to delete this transaction",
+  );
+
+  const handleDelete = async () => {
+    const ok = await confirm();
+
+    if (ok) deleteMutation.mutate();
+  };
+
   return (
     <>
+      <ConfirmDialog />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="size-8 p-0">
@@ -25,9 +40,19 @@ export function Actions({ id }: Props) {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-          <DropdownMenuItem disabled={false} onClick={() => onOpen(id)}>
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={() => onOpen(id)}
+          >
             <Edit className="size-4 mr-2" />
             Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={handleDelete}
+          >
+            <Trash className="size-4 mr-2" />
+            Delete
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet copy";
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 import { useMountedState } from "react-use";
 export function SheetProvider() {
